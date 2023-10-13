9b243c20fc740a51008617f37d12bbe458357f03
hrushabhNano <hrushabh.kale@nanostuffs.com>
2023-10-13 16:48:26 +0530

    first commit after configuring nodejs roite

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+// serving client files from node server
+app.use(express.static("client/dist"));
+
+const path = require("path");
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
+
+app.listen(process.env.PORT || 5001, (req, res) => {
+  console.log(`Server listening on ${process.env.PORT}`);
+});
